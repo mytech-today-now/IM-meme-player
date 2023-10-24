374483f0f6c645d3592369f6ecf923ed25ebdfa2
myTech.Today <mytechtoday@protonmail.com>
2023-10-23 23:30:21 -0500

    modified:   filelist.php

--- filelist.php
+++ filelist.php
@@ -1,45 +1,38 @@
 <?php
 
-// Allowed origins
-$allowed_origins = ["https://insidiousmeme.com", "https://www.insidiousmeme.com"];
-
-// Check the Origin header of the incoming request
-$request_origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-
-if (in_array($request_origin, $allowed_origins, true)) {
-    header("Access-Control-Allow-Origin: $request_origin");
-} else {
-    // If the origin is not allowed, exit early
-    exit('CORS policy violation.');
-}
-
+// CORS headers to allow any origin to access
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header('Content-Type: application/json');
 
-// Configuration
-// NOTE: Update this path to the correct file system path on your server
-$config = [
-    "dirPath" => '/fightth5/public_html/insidiousmeme/presenta/memes/',
-];
-
-try {
-    // Check if the directory exists and is readable
-    if (!is_dir($config["dirPath"]) || !is_readable($config["dirPath"])) {
-        throw new Exception("The specified directory does not exist or is not readable.");
-    }
-
-    // Use glob pattern to get only the required files (assuming only images and videos are required)
-    $files = glob($config["dirPath"] . '*.{jpg,jpeg,jfif,png,gif,mp4,webm,ogg}', GLOB_BRACE);
-
-    // Return the JSON response
-    echo json_encode(["data" => $files]);
-} catch (Exception $e) {
-    // Structured error response without HTML
-    echo json_encode([
-        "status" => "error",
-        "message" => "An error occurred while processing the request: " . $e->getMessage()
-    ]);
+// Exit early if it's an OPTIONS request (pre-flight)
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit;
 }
+
+// Directory URL
+$dirPath = "./presenta/memes/";
+
+// Check if the directory exists
+if (!is_dir($dirPath)) {
+    echo json_encode(["error" => "Directory not found."]);
+    exit;
+}
+
+// Try to get the file names from the directory
+$files = @scandir($dirPath);
+
+// Check if the scandir function succeeded
+if ($files === false) {
+    echo json_encode(["error" => "Failed to read directory contents."]);
+    exit;
+}
+
+// Filter out '.' and '..'
+$files = array_diff($files, array('..', '.'));
+
+// Return the JSON response
+echo json_encode($files);
 
 ?>
