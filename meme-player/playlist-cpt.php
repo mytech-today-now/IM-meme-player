13af75de4291fbd6c043207a0270db75bc922be0
myTech.Today <mytechtoday@protonmail.com>
2023-12-06 16:32:57 -0600

    modified:   meme-player/playlist-cpt.php 	modified:   meme-player/playlist-display.php

--- meme-player/playlist-cpt.php
+++ meme-player/playlist-cpt.php
@@ -2,11 +2,48 @@
 // playlist-cpt.php
 
 function register_playlist_cpt() {
-    $args = array(
-        'public' => true,
-        'label'  => 'Playlists',
-        'supports' => array('title', 'editor', 'custom-fields'),
+    $labels = array(
+        'name'               => 'Playlists',
+        'singular_name'      => 'Playlist',
+        'menu_name'          => 'Playlists',
+        'name_admin_bar'     => 'Playlist',
+        'add_new'            => 'Add New',
+        'add_new_item'       => 'Add New Playlist',
+        'new_item'           => 'New Playlist',
+        'edit_item'          => 'Edit Playlist',
+        'view_item'          => 'View Playlist',
+        'all_items'          => 'All Playlists',
+        'search_items'       => 'Search Playlists',
+        'parent_item_colon'  => 'Parent Playlists:',
+        'not_found'          => 'No playlists found.',
+        'not_found_in_trash' => 'No playlists found in Trash.'
     );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array('slug' => 'playlist'),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => null,
+        'supports'           => array('title', 'editor', 'custom-fields'),
+        'capabilities'       => array(
+            'edit_post'          => 'edit_playlist',
+            'read_post'          => 'read_playlist',
+            'delete_post'        => 'delete_playlist',
+            'edit_posts'         => 'edit_playlists',
+            'edit_others_posts'  => 'edit_others_playlists',
+            'publish_posts'      => 'publish_playlists',
+            'read_private_posts' => 'read_private_playlists'
+        ),
+        'map_meta_cap'       => true
+    );
+
     register_post_type('playlist', $args);
 }
 
--- meme-player/playlist-display.php
+++ meme-player/playlist-display.php
@@ -3,7 +3,38 @@
 
 function display_playlist($playlist_id) {
     $playlist = get_post($playlist_id);
-    // Render the playlist content. This could be a list of items, a media player, etc.
-    // This is a placeholder and should be replaced with actual rendering logic.
-    echo '<div class="playlist">' . $playlist->post_content . '</div>';
+
+    if (!$playlist) {
+        echo "Playlist not found.";
+        return;
+    }
+
+    // Assuming playlist items are stored in a custom field (e.g., 'playlist_items')
+    // Each item could be a post ID, a URL, or any identifier for the media/content
+    $playlist_items = get_post_meta($playlist_id, 'playlist_items', true);
+
+    if (empty($playlist_items)) {
+        echo "No items in this playlist.";
+        return;
+    }
+
+    echo '<div class="playlist">';
+    echo '<h3>' . esc_html($playlist->post_title) . '</h3>';
+    echo '<ul class="playlist-items">';
+
+    foreach ($playlist_items as $item_id) {
+        // Fetch each item details. This could be another post, a media file, etc.
+        // For simplicity, assuming it's another post
+        $item = get_post($item_id);
+        if ($item) {
+            echo '<li class="playlist-item">';
+            echo '<a href="' . esc_url(get_permalink($item_id)) . '">' . esc_html($item->post_title) . '</a>';
+            echo '</li>';
+        }
+    }
+
+    echo '</ul>';
+    echo '</div>';
 }
+?>
+
