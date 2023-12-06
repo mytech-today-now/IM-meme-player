69bcee2d8cdd9e0e855eb576bb7f90b1102b0c9e
myTech.Today <mytechtoday@protonmail.com>
2023-12-06 16:53:54 -0600

    modified:   meme-player/playlist-helpers.php

--- meme-player/playlist-helpers.php
+++ meme-player/playlist-helpers.php
@@ -4,16 +4,33 @@
 // Assuming a database connection is already established
 
 /**
- * Retrieves playlist items from the database.
+ * Retrieves playlist items from the database with optional filtering.
  *
  * @param int $playlist_id The ID of the playlist.
+ * @param string|null $tag Optional tag for filtering.
+ * @param string|null $category Optional category for filtering.
  * @return array The playlist items.
  */
-function get_playlist_items($playlist_id) {
+function get_playlist_items($playlist_id, $tag = null, $category = null) {
     $items = [];
     $query = "SELECT * FROM playlist_items WHERE playlist_id = ?";
+    $params = [$playlist_id];
+    $types = "i";
+
+    if ($tag !== null) {
+        $query .= " AND tag = ?";
+        $params[] = $tag;
+        $types .= "s";
+    }
+
+    if ($category !== null) {
+        $query .= " AND category = ?";
+        $params[] = $category;
+        $types .= "s";
+    }
+
     $stmt = $mysqli->prepare($query);
-    $stmt->bind_param("i", $playlist_id);
+    $stmt->bind_param($types, ...$params);
     $stmt->execute();
     $result = $stmt->get_result();
 
@@ -26,48 +43,22 @@ function get_playlist_items($playlist_id) {
 }
 
 /**
- * Displays playlist items.
- *
- * @param array $items The playlist items.
- */
-function display_playlist_items($items) {
-    echo "<ul>";
-    foreach ($items as $item) {
-        echo "<li>" . htmlspecialchars($item['name']) . "</li>"; // Assuming 'name' is a column in your playlist_items table
-    }
-    echo "</ul>";
-}
-
-/**
- * Adds an item to the playlist.
+ * Adds an item to the playlist with tagging and categorization.
  *
  * @param int $playlist_id The ID of the playlist.
  * @param string $item_name The name of the item to add.
+ * @param string|null $tag Optional tag for the item.
+ * @param string|null $category Optional category for the item.
  */
-function add_playlist_item($playlist_id, $item_name) {
-    $query = "INSERT INTO playlist_items (playlist_id, name) VALUES (?, ?)";
+function add_playlist_item($playlist_id, $item_name, $tag = null, $category = null) {
+    $query = "INSERT INTO playlist_items (playlist_id, name, tag, category) VALUES (?, ?, ?, ?)";
     $stmt = $mysqli->prepare($query);
-    $stmt->bind_param("is", $playlist_id, $item_name);
-    $stmt->execute();
-    $stmt->close();
-}
-
-/**
- * Removes an item from the playlist.
- *
- * @param int $playlist_id The ID of the playlist.
- * @param int $item_id The ID of the item to remove.
- */
-function remove_playlist_item($playlist_id, $item_id) {
-    $query = "DELETE FROM playlist_items WHERE playlist_id = ? AND id = ?";
-    $stmt = $mysqli->prepare($query);
-    $stmt->bind_param("ii", $playlist_id, $item_id);
+    $stmt->bind_param("isss", $playlist_id, $item_name, $tag, $category);
     $stmt->execute();
     $stmt->close();
 }
 
 // Example usage
 $playlist_id = 1; // Example playlist ID
-$items = get_playlist_items($playlist_id);
-display_playlist_items($items);
-
+$items = get_playlist_items($playlist_id, 'funny', 'video');
+// Display or process $items as needed
