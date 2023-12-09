e99f18c495c19cdd768bf7fd0f01a4372e9dabff
myTech.Today <mytechtoday@protonmail.com>
2023-12-08 19:37:16 -0600

    modified:   playlist-display.php 	modified:   playlist-helpers.php

--- playlist-display.php
+++ playlist-display.php
@@ -1,35 +1,46 @@
 <?php
 // playlist-display.php
 
+/**
+ * Displays a playlist based on the provided playlist ID.
+ * 
+ * @param int $playlist_id The ID of the playlist to display.
+ */
 function display_playlist($playlist_id) {
+    // Retrieve the playlist post
     $playlist = get_post($playlist_id);
 
+    // Check if the playlist exists
     if (!$playlist) {
         echo "Playlist not found.";
+        error_log("Playlist with ID $playlist_id not found."); // Log error for debugging
         return;
     }
 
-    // Assuming playlist items are stored in a custom field (e.g., 'playlist_items')
-    // Each item could be a post ID, a URL, or any identifier for the media/content
+    // Retrieve playlist items, assuming they are stored in a custom field 'playlist_items'
     $playlist_items = get_post_meta($playlist_id, 'playlist_items', true);
 
+    // Check if the playlist has items
     if (empty($playlist_items)) {
         echo "No items in this playlist.";
         return;
     }
 
+    // Display the playlist
     echo '<div class="playlist">';
     echo '<h3>' . esc_html($playlist->post_title) . '</h3>';
     echo '<ul class="playlist-items">';
 
+    // Iterate over each item in the playlist
     foreach ($playlist_items as $item_id) {
-        // Fetch each item details. This could be another post, a media file, etc.
-        // For simplicity, assuming it's another post
+        // Fetch details for each item, assuming it's another post
         $item = get_post($item_id);
         if ($item) {
             echo '<li class="playlist-item">';
             echo '<a href="' . esc_url(get_permalink($item_id)) . '">' . esc_html($item->post_title) . '</a>';
             echo '</li>';
+        } else {
+            error_log("Playlist item with ID $item_id not found."); // Log error for missing items
         }
     }
 
@@ -37,4 +48,3 @@ function display_playlist($playlist_id) {
     echo '</div>';
 }
 ?>
-
--- playlist-helpers.php
+++ playlist-helpers.php
@@ -1,79 +1,67 @@
 <?php
 // playlist-helpers.php
 
-// Assuming a database connection is already established
+global $wpdb; // Use the global WordPress database object
 
 /**
  * Retrieves playlist items from the database with optional filtering.
  *
- * @param mysqli $mysqli The database connection object.
  * @param int $playlist_id The ID of the playlist.
  * @param string|null $tag Optional tag for filtering.
  * @param string|null $category Optional category for filtering.
  * @return array The playlist items.
  */
-function get_playlist_items($mysqli, $playlist_id, $tag = null, $category = null) {
+function get_playlist_items($playlist_id, $tag = null, $category = null) {
+    global $wpdb;
     $items = [];
-    $query = "SELECT * FROM playlist_items WHERE playlist_id = ?";
+    $query = "SELECT * FROM {$wpdb->prefix}playlist_items WHERE playlist_id = %d";
     $params = [$playlist_id];
-    $types = "i";
 
     if ($tag !== null) {
-        $query .= " AND tag = ?";
+        $query .= " AND tag = %s";
         $params[] = $tag;
-        $types .= "s";
     }
 
     if ($category !== null) {
-        $query .= " AND category = ?";
+        $query .= " AND category = %s";
         $params[] = $category;
-        $types .= "s";
     }
 
-    if ($stmt = $mysqli->prepare($query)) {
-        $stmt->bind_param($types, ...$params);
-        if ($stmt->execute()) {
-            $result = $stmt->get_result();
+    $prepared_query = $wpdb->prepare($query, $params);
+    $results = $wpdb->get_results($prepared_query, ARRAY_A);
 
-            while ($row = $result->fetch_assoc()) {
-                $items[] = $row;
-            }
-        } else {
-            error_log("Error executing query: " . $stmt->error);
-        }
-        $stmt->close();
-    } else {
-        error_log("Error preparing statement: " . $mysqli->error);
+    if ($results === null) {
+        // Log error if query fails
+        error_log("Error executing query: " . $wpdb->last_error);
+        return [];
     }
 
-    return $items;
+    return $results;
 }
 
 /**
  * Adds an item to the playlist with tagging and categorization.
  *
- * @param mysqli $mysqli The database connection object.
  * @param int $playlist_id The ID of the playlist.
  * @param string $item_name The name of the item to add.
  * @param string|null $tag Optional tag for the item.
  * @param string|null $category Optional category for the item.
  */
-function add_playlist_item($mysqli, $playlist_id, $item_name, $tag = null, $category = null) {
-    $query = "INSERT INTO playlist_items (playlist_id, name, tag, category) VALUES (?, ?, ?, ?)";
+function add_playlist_item($playlist_id, $item_name, $tag = null, $category = null) {
+    global $wpdb;
+    $query = "INSERT INTO {$wpdb->prefix}playlist_items (playlist_id, name, tag, category) VALUES (%d, %s, %s, %s)";
     
-    if ($stmt = $mysqli->prepare($query)) {
-        $stmt->bind_param("isss", $playlist_id, $item_name, $tag, $category);
-        if (!$stmt->execute()) {
-            error_log("Error executing query: " . $stmt->error);
-        }
-        $stmt->close();
-    } else {
-        error_log("Error preparing statement: " . $mysqli->error);
+    $prepared_query = $wpdb->prepare($query, $playlist_id, $item_name, $tag, $category);
+    $result = $wpdb->query($prepared_query);
+
+    if ($result === false) {
+        // Log error if query fails
+        error_log("Error executing query: " . $wpdb->last_error);
     }
 }
 
 // Example usage
-$mysqli = new mysqli("host", "user", "password", "database"); // Replace with actual database connection details
 $playlist_id = 1; // Example playlist ID
-$items = get_playlist_items($mysqli, $playlist_id, 'funny', 'video');
+$items = get_playlist_items($playlist_id, 'funny', 'video');
 // Display or process $items as needed
+?>
